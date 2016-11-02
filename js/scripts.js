fc763bd35f67fa923d950d73e8208acec36fa00a
doohinkus <doohinkus@gmail.com>
2016-11-01 22:31:00 -0700

    not fix the memory problem.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,14 @@ function BankAccount(name, amount) {
 
 }
 
+BankAccount.prototype.initialDeposit = function (deposit){
+  return this.amount += deposit;
+  // console.log("function: ", this.amount);
+}
 
 BankAccount.prototype.deposit = function (deposit){
   return this.amount = deposit + this.amount;
-  console.log("function: ", this.amount);
+  // console.log("function: ", this.amount);
 }
 
 BankAccount.prototype.withdrawal = function (withdrawal){
@@ -23,38 +27,35 @@ BankAccount.prototype.withdrawal = function (withdrawal){
 
 
 $(document).ready(function (){
-var account = new BankAccount("","");
-  $("#form1").click(function (event){
-    event.preventDefault();
-
-    var userName = $("#userName").val();
-    var userInitialBalance = parseInt($("#userInitialBalance").val());
-
-    account.name = userName;
-    account.amount = userInitialBalance;
-
-    console.log(account.amount);
-//set price here, using values from our array
-
-
-    $("#runningBalanceWell").text(account.amount);
-    //call reset function
-    $("#openingBalance").hide();
-    $("#userNameHead").text(account.name);
-
-  });
 
   $("#form2").click(function (event){
     event.preventDefault();
+    var userName = $("#userName").val();
+    var userInitialBalance = parseInt($("#userInitialBalance").val());
+    var account = new BankAccount(userName, 0);
 
-    var userDeposit = parseInt($("#userDeposit").val());
-    var userWithdrawal = parseInt($("#userWithdrawal").val());
 
-    // var newBalance = new BankAccount("", );
-    console.log(account.deposit(userDeposit));
-    console.log(account.withdrawal(userWithdrawal));
-
+    $("#openingBalance").hide();
+    $("#register").fadeIn();
+    account.deposit(parseInt($("#userInitialBalance").val()));
+    account.withdrawal(parseInt($("#userWithdrawal").val()));
     $("#runningBalanceWell").text(account.amount);
+    $("#userNameHead").text(account.name);
+
+    //$("#userDeposit").val(0);
+
+
+
+
   });
+
+  // $("#withdrawalButton").click(function (event){
+  // event.preventDefault();
+  // console.log(account.withdrawal(parseInt($("#userWithdrawal").val())), "sds");
+  // $("#runningBalanceWell").text(account.amount);
+  // $("#userNameHead").text(account.name);
+  // });
+
+
 
 });
